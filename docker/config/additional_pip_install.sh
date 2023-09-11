67962aa453dafc8a90189c33cf4f8462e805f52e
Ben <hj.ben.kim@gmail.com>
2023-09-11 23:06:38 +0900

    Update additional_pip_install.sh

--- docker/config/additional_pip_install.sh
+++ docker/config/additional_pip_install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 pip install stemgraphic
+pip install kendall-w
+pip install gower
+pip install hdbscan
+pip install scikit-learn-extra
+pip install minisom
+pip install factor_analyzer
